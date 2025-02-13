48d7ddee6133a4477c064bd2dc08841854850fba
Kevin Hannan <kphannan@gmail.com>
2025-02-12 20:47:43 -0500

    feat: add code for app and single library

---
+++ app/src/main/cpp/src/foo.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+#include <iomanip>
+
+#include "bar.hpp"
+
+int main(int argc, const char **argv)
+{
+    std::cout << "Hello world\n";
+
+    barFN();
+}
---
+++ libs/lib1/src/main/cpp/src/bar.cpp
@@ -0,0 +1,8 @@
+
+#include <iostream>
+#include <iomanip>
+
+void barFN()
+{
+    std::cout << "bar function\n";
+}
